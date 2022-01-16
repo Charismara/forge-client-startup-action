ad6d1b8b629fc75a5db06b4096c7cc9885b91b2c
Christian Raoulis <craoulis@googlemail.com>
2022-01-16 13:02:35 +0100

    execute runClient task

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -12,4 +12,9 @@ repositoryGit=${repositoryURL##*/}
 repositoryName=${repositoryGit::-4}
 echo ">>> Identified $repositoryName as work dir"
 cd $repositoryName
-ls
+chmod +x gradlew
+echo ">>> Starting Client"
+./gradlew runClient
+echo ">>> Waiting for exit message"
+#Created: 128x128x0 minecraft:textures/atlas/mob_effects.png-atlas
+grep -p 'Created: 128x128x0 minecraft:textures/atlas/mob_effects.png-atlas' < tail -f run/logs/latest.log || exit 1
